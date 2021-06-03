8c4d542f4faf14f99545e67cbaef6bdeb14416bc
maggie <maggie.tamanini@gmail.com>
2021-06-02 21:34:48 -0400

    Added a try catch to deal with exceptions.

--- Program.cs
+++ Program.cs
@@ -7,49 +7,56 @@ namespace PowersTable
         static void Main(string[] args)
         {
             bool goOn = true;
-            while(goOn == true)
+            while (goOn == true)
+            {
+                try
                 {
                 string num = GetUserInput("Please enter a whole number from 1 to 1290.");
-                Console.WriteLine(num);
+                Console.WriteLine($"You entered: {num}");
                 int num1 = int.Parse(num);
-
-                if(num1 >=1 && num1 <= 1290)
-                { 
-                Console.WriteLine("Number\tSquared\tCubed");
-                Console.WriteLine("=====\t=====\t======");
-
-                for (int c = 1; c <= num1; c++)
-                {
-                    Console.WriteLine($"{c}   \t{c * c }   \t{c * c * c}");
-                }
-
-                bool correctInput = true;
-                    while (correctInput == true)
+                    if (num1 >= 1 && num1 <= 1290)
                     {
-                        Console.WriteLine("Would you like to enter another number? Y/N");
-                        string input = Console.ReadLine();
-                        if (input.ToLower() == "y")
-                        {
-                            correctInput = false;
-                            goOn = true;
-                        }
-                        else if (input.ToLower() == "n")
-                        {
-                            Console.WriteLine("OK!  Thanks for playing!");
-                            goOn = false;
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("I'm sorry, I didn't understand, please try again.");
+                        Console.WriteLine("Number\tSquared\tCubed");
+                        Console.WriteLine("=====\t=====\t======");
 
+                        for (int c = 1; c <= num1; c++)
+                        {
+                            Console.WriteLine($"{c}   \t{c * c }   \t{c * c * c}");
+                        }
+
+                        bool correctInput = true;
+                        while (correctInput == true)
+                        {
+                            Console.WriteLine("Would you like to enter another number? Y/N");
+                            string input = Console.ReadLine();
+                            if (input.ToLower() == "y")
+                            {
+                                correctInput = false;
+                                goOn = true;
+                            }
+                            else if (input.ToLower() == "n")
+                            {
+                                Console.WriteLine("OK!  Thanks for playing!");
+                                goOn = false;
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("I'm sorry, I didn't understand, please try again.");
+
+                            }
                         }
                     }
-                }
-                else
-                { Console.WriteLine("That is not a whole number between 1 and 1290.");
-                    goOn = true;
+                    //else
+                    //{
+                    //    Console.WriteLine("That is not a whole number between 1 and 1290.");
+                    //    goOn = true;
 
+                    //}
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("That is not a whole number.");
                 }
 
             }
